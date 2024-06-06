ee5e4a5700f0504ec536c8f03b1ddf6ea9443ed4
UdayPrakashk5 <164043927+UdayPrakashk5@users.noreply.github.com>
2024-06-06 21:02:09 +0530

    Price Paging Project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            alert(`You have selected the ${this.parentElement.querySelector('h2').textContent} plan.`);
+        });
+    });
+});
+
