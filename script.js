630f329ffc928ab8f1198bfeb15cb8a0380857bc
UdayPrakashk5 <164043927+UdayPrakashk5@users.noreply.github.com>
2024-06-06 20:20:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert(You have selected the ${this.parentElement.querySelector('h2').textContent} plan.);
-        });
-    });
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Product added to cart!');
 });
